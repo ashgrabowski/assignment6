a13ed9d49adb3370ae3fc4d0b4d498c621b5e00c
AshleyGrabowski <agrabowski@ufl.edu>
2019-04-29 23:44:34 -0400

    Javascript changes

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+//Array
+var members = ['Dave Grohl', 'Nate Mendel', 'Pat Smear', 'Taylor Hawkins', 'Rami Jaffee', 'Chris Shiflett'];
+
+//Object
+var tour = {
+  Chicago: "July 29th and 30th, 2018",
+  Seattle: "September 1st, 2018",
+  Edmonton: "September 4th, 2018",
+}
+
+//While loop
+var i = 0;
+
+while (i < 5) {
+  console.log('Times running through the loop: ') + i;
+
+  i++;
+}
